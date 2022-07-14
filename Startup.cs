5f2884e2e9409f7cf18d9be45fee98926196d9bd
Richard Njembele <richardthierynjembele@gmail.com>
2022-07-14 10:00:06 +0100

    Mise à jour Template

--- Startup.cs
+++ Startup.cs
@@ -87,7 +87,7 @@ namespace FirstProjetNRT
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Login}/{action=Index}/{id?}");
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
